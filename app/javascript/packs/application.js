1d4dab3b417600f5a8c171ed7c69873340bc064c
erpaffo <116151302+erpaffo@users.noreply.github.com>
2024-08-12 17:02:17 +0200

    Fixed DELETE routes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,10 @@ import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
 import $ from 'jquery';
 import Turbolinks from "turbolinks";
+import Rails from "@rails/ujs"
+
+Rails.start()
+
 Turbolinks.start();
 
 ActiveStorage.start();
