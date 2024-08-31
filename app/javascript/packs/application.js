c08a61e800f8293066bed0d6189723a87db881a4
erpaffo <116151302+erpaffo@users.noreply.github.com>
2024-08-31 15:41:46 +0200

    Improved gallery view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -38,27 +38,3 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-// gallery view
-document.addEventListener('DOMContentLoaded', () => {
-  const slider = document.getElementById('project-slider');
-  const prevBtn = document.getElementById('prev-btn');
-  const nextBtn = document.getElementById('next-btn');
-  
-  const scrollAmount = slider.clientWidth; // How much to scroll with each click
-
-  let scrollCount = 0;
-
-  prevBtn.addEventListener('click', () => {
-    if (scrollCount > 0) {
-      slider.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-      scrollCount--;
-    }
-  });
-
-  nextBtn.addEventListener('click', () => {
-    if (scrollCount < 10) { // limit to 10 scrolls
-      slider.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-      scrollCount++;
-    }
-  });
-});
